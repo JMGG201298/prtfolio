167c6191ab513b2051b87ae405214cb2c5d7f5ae
Juan Manuel <s17120263@alumnos.itsur.edu.mx>
2021-12-19 14:42:47 -0600

    Se agrego la sección de educación

--- js/js.js
+++ js/js.js
@@ -3,7 +3,19 @@ window.onload=()=>{
     setInterval(()=>{
         document.body.className="";
         modalCarga.className="noScroll-none";
+        setInterval(()=>{
+            modalCarga.style.display="none";
+        },1000);
 
     },3000);
-    
+
+    let btnItsur=document.querySelector("#btnItsur");
+    let btnCbtis=document.querySelector("#btnCbtis");
+
+    btnItsur.addEventListener("click",(e)=>{
+        window.open("http://www.itsur.edu.mx/");
+    });
+    btnCbtis.addEventListener("click",(e)=>{
+        window.open("http://www.itsur.edu.mx/");
+    });
 }
